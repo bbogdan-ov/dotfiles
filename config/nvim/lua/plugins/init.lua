9965324ff09270195c6a90ed7d5ad5f6306b0053
bogdanov <bbogdan_ov@mail.ru>
2024-04-21 14:07:53 +0300

    catch missing plugin config

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -13,11 +13,17 @@ for _, plugin in ipairs(list) do
 	end
 	if type(plugin_conf) == "string" then
 		plugin.config = function()
-			local is_ok, _ = pcall(require, "plugins.configs." .. plugin_conf)
+			local is_ok, err = pcall(require, "plugins.configs." .. plugin_conf)
 
 			if not is_ok then
 				-- TODO: catch missing module
-				require(plugin_conf).setup {}
+				local is_ok, module = pcall(require, plugin_conf)
+
+				if is_ok then
+					module.setup {}
+				else
+					vim.api.nvim_err_writeln(string.format('Cannot load plugin config "%s"!\nFIRST ERROR: %s;\nSECOND ERROR: %s', plugin_conf, err, module))
+				end
 			end
 		end
 	end
